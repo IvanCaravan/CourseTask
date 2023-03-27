25db4bc97f7e7bfdb109d0f42db5cee0f277dbee
IvanCaravan <ivan.kirsanov.2000@mail.ru>
2023-03-27 21:10:14 +0300

    please check my course task!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] cities = {"Kiev", "Moscow", "Beijing", "Oslo", "Oslo", "Beijing"};
+        String result = "";
+        for (int i = 0; i < cities.length; i++) { //пишем цикл для сравнения одного элемента со всеми
+            for (int j = 0; j < cities.length;j++){ //пишем вложенный цикл, чтоб перебирать второе значение.
+                if (i!=j && cities[i].equals(cities[j])) { // пишем условный оператор, который будет осуществлять замену
+                    cities[j] = ""; //собственно, сама замена
+                }
+            }
+            result += cities[i] + " "; //конкатенация строк. Сравниваемое значение прошло проверки и пишется в строку.
+        }
+        System.out.println(result); //выводим эту строку
     }
 }
